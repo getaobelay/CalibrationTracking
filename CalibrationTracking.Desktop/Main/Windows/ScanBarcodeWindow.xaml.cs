f20cb62f13a292c7e7522e3f442cbc255cfef958
Getao Belay <Getao-B.ext@tadiran-batt.com>
2022-11-17 12:09:07 +0200

    fixed focus

--- CalibrationTracking.Desktop/Main/Windows/ScanBarcodeWindow.xaml.cs
+++ CalibrationTracking.Desktop/Main/Windows/ScanBarcodeWindow.xaml.cs
@@ -5,7 +5,6 @@ using System;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 
 namespace CalibrationTracking.Desktop.Main.Windows
@@ -177,41 +176,3 @@ namespace CalibrationTracking.Desktop.Main.Windows
     }
 }
 
-namespace CalibrationTracking.Desktop.Main.Windows
-{
-    public static class FocusBehavior
-    {
-        public static readonly DependencyProperty FocusFirstProperty =
-            DependencyProperty.RegisterAttached(
-                "FocusFirst",
-                typeof(bool),
-                typeof(FocusBehavior),
-                new PropertyMetadata(false, OnFocusFirstPropertyChanged));
-
-        public static bool GetFocusFirst(Control control)
-        {
-            return (bool)control.GetValue(FocusFirstProperty);
-        }
-
-        public static void SetFocusFirst(Control control, bool value)
-        {
-            control.SetValue(FocusFirstProperty, value);
-        }
-
-        private static void OnFocusFirstPropertyChanged(
-            DependencyObject obj, DependencyPropertyChangedEventArgs args)
-        {
-            Control control = obj as Control;
-            if (control == null || !(args.NewValue is bool))
-            {
-                return;
-            }
-
-            if ((bool)args.NewValue)
-            {
-                control.Loaded += (sender, e) =>
-                    control.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
-            }
-        }
-    }
-}
